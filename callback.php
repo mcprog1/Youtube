cd1f78f0a00be0c00e43935f3bc8d4ac835c5afc
Enzo Nicolas Stagno Piriz <nicolasstagno@cras-dev.com>
2020-03-16 16:47:32 -0300

    Se agrego todo

---
+++ callback.php
@@ -0,0 +1,22 @@
+<?php
+    require_once 'config.php';
+
+    if(isset($_SESSION['access_token_google']))
+    {
+        $gClient->setAccessToken($_SESSION['access_token_google']);
+    }else if(isset($_GET['code'])) 
+    {
+        $token = $gClient->fetchAccessTokenWithAuthCode($_GET['code']);
+        $_SESSION['access_token_google'] = $token;
+    }else{
+        header('Location: web/login.php');
+        exit();
+    }
+
+
+    $oAuth = new Google_Service_Oauth2($gClient);
+    $userData = $oAuth->userinfo_v2_me->get();
+    
+
+
+?>
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    require_once 'GoogleAPI/vendor/autoload.php';
+    $gClient = new Google_Client();
+    $gClient->setClientId("129659152028-hlaens37noe9ksr1o8m0vbik859nba77.apps.googleusercontent.com");
+    $gClient->setClientSecret("AIzaSyAqb1oY3qtiavR6tggKMuEzPB7MscBRgv8");
+    $gClient->setApplicationName("SubirYoutube");
+    $gClient->setRedirectUri("http://localhost/youtube/callback.php");
+    //SCOPE SON LOS PERMIOSOS
+    $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/");
+
+?>
